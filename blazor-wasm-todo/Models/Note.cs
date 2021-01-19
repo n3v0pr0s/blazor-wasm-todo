1ba060c20933b60c5dcd954720678ff020f270c3
n3v0pr0s <n3v0pr0s@gmail.com>
2021-01-19 22:14:43 +0500

    added note component, model with fake data and index page view

---
+++ blazor-wasm-todo/Models/Note.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace blazor_wasm_todo.Models
+{
+    public class Note
+    {
+        public DateTime DateTime { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public Note(string title)
+        {
+            this.Title = title;
+            this.DateTime = DateTime.Now;
+        }
+
+        //why binding for input not working
+        public Note(string title, bool done)
+        {
+            this.Title = title;
+            this.IsDone = done;
+            this.DateTime = DateTime.Now;
+        }
+
+        public static List<Note> GetData()
+        {
+            return new List<Note>()
+            {
+                new Note("Купить хлеб", true),
+                new Note("Дочитать книгу"),
+                new Note("Выучить ангуляр"),
+                new Note("Познать цзен")
+            };
+        }
+    }
+}
+
+
+
