678e7296389add20a4843a90a2e2b92ba36f0231
n3v0pr0s <n3v0pr0s@gmail.com>
2021-01-28 01:07:48 +0500

    added sidebar with boards

---
+++ blazor-wasm-todo/Models/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace blazor_wasm_todo.Models
+{
+    public class Board
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Note> Notes { get; set; }
+
+        public Board(string name)
+        {
+            this.Guid = Guid.NewGuid();
+            this.Name = name;
+            this.Notes = new List<Note>();
+        }
+
+        public static List<Board> GetBoards()
+        {
+            return new List<Board>
+            {
+                new Board("Pet-projects"),
+                new Board("Production"),
+                new Board("Technologies"),
+                new Board("Books")
+
+            };
+        }
+    }
+}
